7b220ffd0dc40e452ea11552a03ef90512330457
AlekseiSolodukha <alekseisoloduha@gmail.com>
2022-05-23 20:13:05 +0300

    feat: add updating of copy-file folder by using fs.unlink

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,13 +1,20 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 async function makeDirCopy(){
   const pathToFiles = path.join(__dirname, 'files');
-  const pathToFilesCopy = path.join(__dirname, 'files-copy'); 
-  await fs.promises.mkdir(pathToFilesCopy, {recursive: true});
-  const files = await fs.promises.readdir(pathToFiles);
+  const pathToFilesCopy = path.join(__dirname, 'files-copy');
+
+  await fs.mkdir(pathToFilesCopy, {recursive: true});
+  const coppiedfiles = await fs.readdir(pathToFilesCopy);
+
+  coppiedfiles.forEach(async (file) => {
+    await fs.unlink(path.join(pathToFilesCopy, file));
+  });
+
+  const files = await fs.readdir(pathToFiles);
   files.forEach(async (file) => {
-    await fs.promises.copyFile(path.join(pathToFiles, file), path.join(pathToFilesCopy, file));
+    await fs.copyFile(path.join(pathToFiles, file), path.join(pathToFilesCopy, file));
   });
 }
 
